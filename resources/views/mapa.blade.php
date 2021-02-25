8bd44f6d64040f8d21d04ddf74efdd71cc9e57e5
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2021-02-25 12:57:04 +0100

    mapbox token

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -33,6 +33,14 @@
 <section class="bg-white text-gray-800">
 {{--    --}}
 
+    <div id='map' style='width: 400px; height: 300px;'></div>
+    <script>
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzYXRvcjAwIiwiYSI6ImNrbGtueXNmaDBnb3Qyd3AyaDVndHBhbTYifQ.8QQ15tpipTT07pjLjkVaww';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    </script>
 
 </section>
 
