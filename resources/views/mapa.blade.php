3aedfd270e4c3002a6869440ca70d1db1d78b7db
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2021-02-25 12:45:52 +0100

    mergeo

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -31,14 +31,7 @@
 
 {{-- Aquí va el contenido del centro --}}
 <section class="bg-white text-gray-800">
-    <div id='map' style='width: 400px; height: 300px;'></div>
-    <script>
-        mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzYXRvcjAwIiwiYSI6ImNrbGtueXNmaDBnb3Qyd3AyaDVndHBhbTYifQ.8QQ15tpipTT07pjLjkVaww';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11'
-        });
-    </script>
+{{--    --}}
 
 
 </section>
