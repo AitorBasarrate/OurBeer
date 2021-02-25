444489373ceb5c65eef91fe17de6d700fd7bf0c8
MarkelMiranda <markel.miranda1@gmail.com>
2021-02-25 11:59:55 +0100

    asdasd

--- app/Http/Controllers/SuperController.php
+++ app/Http/Controllers/SuperController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class SuperController extends Controller
 {
-    //
+    public function welcome(){
+        return view('welcome');
+    }
+
+    public function mapa(){
+        return view('mapa');
+    }
+
+    public function lista(){
+        return view('lista');
+    }
 }
