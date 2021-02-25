cce388718504ed86f0a0617625ec17a841017b75
KaietOrza <kaiet.orza@gmail.com>
2021-02-25 12:41:34 +0100

    La subicion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,15 @@
         <!--Left Col-->
         <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
           <h1 class="my-4 text-5xl font-bold leading-tight">
-            Encuentra tu Cerveza favorita en cualquier sitio?
+            Encuentra tu Cerveza favorita en cualquier sitio.
           </h1>
           <p class="leading-normal text-2xl mb-8">
             Localiza los cañeros, y los diferentes tipos de cervezas en los bares de tu alrededor.
           </p>
-          {{-- <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-            Subscribe
-          </button> --}}
         </div>
         <!--Right Col-->
-        <div class="w-full md:w-3/5 py-6 text-center">
-          <img class="w-full md:w-4/5 z-50" src="../img/Cañeros.jpg" />
+        <div class="w-full md:w-3/5 py-6">
+          <img class="w-full md:w-4/5 z-50" src="../img/cerveza.png" />
         </div>
       </div>
     </div>
